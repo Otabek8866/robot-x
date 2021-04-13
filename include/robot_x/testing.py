dd6999c7ab4aff16f2397877ead8b3f624a3d16d
Otabek Sobirov <sobirovotabek1995@gmail.com>
2021-04-13 15:53:07 +0100

    Update testing.py

--- include/robot_x/testing.py
+++ include/robot_x/testing.py
@@ -1,6 +1,8 @@
+#This file is to keep the file structucre
+#when catkin_make command is called, there will be an error if there is no "include" folder
+
 import os
 import time
-
 
 for i in range(30):
 	print('hello world')
